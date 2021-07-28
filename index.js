bb9b987fdb87952479fd4de4ff95de9731fbc0f6
Lee Chuan Xin <chuanxin@gametize.com>
2021-07-29 03:10:32 +0800

    refactor: hexToRgb and rgbToHex functions.

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 const CONVERSION_STRING = process.argv[2];
 const PARAM = process.argv[3];
-let hexArr = [];
-let rgbArr = [];
 let rgbStr = '';
 let hexStr = '';
 
+const hexToRgb = (string) => {
+  // removes "#", and splits string into string array up to 2 characters each
+  const HEX_ARR = PARAM
+    .substring(1)
+    .match(/.{1,2}/g);
+  // convert each string from hexadecimal to decimal
+  const RGB_ARR = HEX_ARR.map(hexNumStr => parseInt(hexNumStr, 16)); 
+  return `rgb(${RGB_ARR.join(',')})`;
+}
+
+const rgbToHex = (string) => {
+  // forms an array of stringified numbers, up to 3 digits each
+  const RGB_ARR = string.match(/\d{1,3}/g);
+  // converts each stringified number into hex, and combines them with "#"
+  const HEX_STR = "#" + RGB_ARR
+    .map(rgb => {
+      const HEX_NUMBER = Number(rgb).toString(16);
+      if (HEX_NUMBER.length < 2) {
+        return `0${HEX_NUMBER}`
+      }
+      return HEX_NUMBER;
+    })
+    .join("");
+  return HEX_STR;
+}
+
 switch(CONVERSION_STRING) {
   case "rgbhex":
-    // forms an array of stringified numbers, up to 3 digits each
-    rgbArr = PARAM.match(/\d{1,3}/g);
-    // converts each stringified number into hex, and combines them with "#"
-    hexStr = "#" + rgbArr
-      .map(rgb => {
-        const HEX_NUMBER = Number(rgb).toString(16);
-        if (HEX_NUMBER.length < 2) {
-          return `0${HEX_NUMBER}`
-        }
-        return HEX_NUMBER;
-      })
-      .join("");
+    hexStr = rgbToHex(PARAM);
     console.log(hexStr);
     break;
+  case "rgbhsl":
+  case "hexhsl":
   case "hexrgb":
   default:
-    // removes "#", and splits string into string array up to 2 characters each
-    hexArr = PARAM
-      .substring(1)
-      .match(/.{1,2}/g);
-    // convert each string from hexadecimal to decimal
-    rgbArr = hexArr.map(hexNumStr => parseInt(hexNumStr, 16)); 
-    rgbStr = `rgb(${rgbArr.join(',')})`;
+    rgbStr = hexToRgb(PARAM);
     console.log(rgbStr);
+    break;
 }
