200721511b6922517ae2f4d58a18bf346258b9cc
Lee Chuan Xin <chuanxin@gametize.com>
2021-07-29 02:56:16 +0800

    feat: rgb to hex, and vice versa.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+const CONVERSION_STRING = process.argv[2];
+const PARAM = process.argv[3];
+let hexArr = [];
+let rgbArr = [];
+let rgbStr = '';
+let hexStr = '';
+
+if (CONVERSION_STRING === "rgbhex") {
+  rgbArr = PARAM.match(/\d{1,3}/g);
+  hexStr = "#" + rgbArr
+    .map(rgb => {
+      const HEX_NUMBER = Number(rgb).toString(16);
+      if (HEX_NUMBER.length < 2) {
+        return `0${HEX_NUMBER}`
+      }
+      return HEX_NUMBER;
+    })
+    .join("");
+  console.log(hexStr);
+} else {
+  hexArr = PARAM
+    .substring(1)
+    .match(/.{1,2}/g);
+  rgbArr = hexArr.map(hexNumStr => parseInt(hexNumStr, 16)); 
+  rgbStr = `rgb(${rgbArr.join(',')})`;
+  console.log(rgbStr);
+}
