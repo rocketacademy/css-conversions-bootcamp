21eb65ec1ebe7553632734bf7ef97e99d422e481
makck <joel.mak32@gmail.com>
2021-09-26 00:18:39 +0800

    Base

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hello world');
+function hexToRgb(hex) {
+  return ['0x' + hex[1] + hex[2] | 0, '0x' + hex[3] + hex[4] | 0, '0x' + hex[5] + hex[6] | 0];
+}
+
+console.log(`${hexToRgb(process.argv[2])}`);
