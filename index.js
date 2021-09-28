915524c00c7451bdd64b3ea3a80846ae11983234
Kimmi Lee <lee.kimmixq@gmail.com>
2021-09-29 01:52:40 +0900

    completed comfortable

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const colHex = process.argv[2];
-const RGBArray = [];
-for (let i = 1; i < colHex.length; i += 2) {
-  const oneRGB = parseInt(colHex.slice(i, i+2), 16);
-  RGBArray.push(oneRGB);
+if (process.argv[2] === 'hexrgb') {
+  const colHex = process.argv[3];
+  let colRGB = 'rgb('
+  for (let i = 1; i < colHex.length; i += 2) {
+    colRGB += parseInt(colHex.slice(i, i+2), 16);
+    if (i === colHex.length - 2) {
+      colRGB += ')'
+    } else {
+      colRGB += ','
+    }
+  }
+  console.log(colRGB);
+} else if (process.argv[2] === 'rgbhex') {
+  const colRGB = process.argv[3].slice(4, -1).split(',');
+  let colHex = '#';
+  for (let i = 0; i < colRGB.length; i += 1) {
+    const oneHex = Number(colRGB[i]).toString(16);
+    colHex += oneHex;
+  }
+  console.log(colHex);
 }
-const colRGB = `rgb(${RGBArray[0]},${RGBArray[1]},${RGBArray[2]})`
-console.log(colRGB);
-
-
