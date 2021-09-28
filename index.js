405bc3ca49b3bd5126ee3917fcf2e4222356c818
Kimmi Lee <lee.kimmixq@gmail.com>
2021-09-29 01:31:59 +0900

    completed base

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const colHex = process.argv[2];
+const RGBArray = [];
+for (let i = 1; i < colHex.length; i += 2) {
+  const oneRGB = parseInt(colHex.slice(i, i+2), 16);
+  RGBArray.push(oneRGB);
+}
+const colRGB = `rgb(${RGBArray[0]},${RGBArray[1]},${RGBArray[2]})`
+console.log(colRGB);
+
+
