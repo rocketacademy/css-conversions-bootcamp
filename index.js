29304776563ac33777ab8afd70acd30c3b623234
Kimmi Lee <lee.kimmixq@gmail.com>
2021-09-29 02:16:08 +0900

    added auto-detect input colour format

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-if (process.argv[2] === 'hexrgb') {
-  const colHex = process.argv[3];
+// converts input from hex to RGB
+const hexToRGB = (input) => {
   let colRGB = 'rgb('
-  for (let i = 1; i < colHex.length; i += 2) {
-    colRGB += parseInt(colHex.slice(i, i+2), 16);
-    if (i === colHex.length - 2) {
+  for (let i = 1; i < input.length; i += 2) {
+    // slice into xx | xx | xx , convert to base 10
+    colRGB += parseInt(input.slice(i, i+2), 16);
+    
+    // add comma, unless at last set add parenthesis
+    if (i === input.length - 2) {
       colRGB += ')'
     } else {
       colRGB += ','
     }
   }
-  console.log(colRGB);
-} else if (process.argv[2] === 'rgbhex') {
-  const colRGB = process.argv[3].slice(4, -1).split(',');
+  return colRGB;
+}
+
+// converts input from RGB to hex
+const RGBToHex = (input) => {
+  // get values in parenthesis, store as array [xx, xx, xx]
+  const colRGB = input.slice(4, -1).split(',')
   let colHex = '#';
   for (let i = 0; i < colRGB.length; i += 1) {
-    const oneHex = Number(colRGB[i]).toString(16);
+    // convert to base 16, pad '0' at start for single digit outcomes
+    let oneHex = Number(colRGB[i]).toString(16);
+    oneHex = oneHex.padStart(2,0);
     colHex += oneHex;
   }
-  console.log(colHex);
+  return colHex;
+}
+
+const userInput = process.argv[2]
+if (userInput[0] === '#' && userInput.length === 7) {
+  console.log('Hex to RGB:', userInput, '->', hexToRGB(userInput))
+} else if (userInput.slice(0, 4) === 'rgb(' && userInput.slice(-1) === ')') {
+  console.log('RGB to Hex:', userInput, '->', RGBToHex(userInput));
+} else {
+  console.log('Invalid input')
 }
