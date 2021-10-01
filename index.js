6c258e4a8b018df7e57e05a8aa02740c88851eec
Graham <graham.lim88@gmail.com>
2021-10-01 23:25:15 +0800

    base done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hello world');
+console.log("this is node process argv for css conversions!");
+console.log(process.argv);
+
+const hexToRgb = hex =>
+  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+    .substring(1).match(/.{2}/g)
+    .map(x => parseInt(x, 16))
+
+console.log(hexToRgb("#0033ff")) // [0, 51, 255]
+console.log(hexToRgb("#03f")) // [0, 51, 255]
+
+// hex to rgb conditional statement
+if (process.argv[2] === "hex2rgb"){
+  console.log("hex to RGB conversion detected");
+  console.log(typeof(process.argv[3]))
+  hexToRgb(process.argv[3])  
+}
+
+// // rgb to hex conditional statement
+// else if (process.argv[2] === "rgb2hex"){
+//   console.log("RGB to hex conversion detected");
+//   rgbToHex(process.argv[3], process.argv[4], process.argv[5])
+// }
