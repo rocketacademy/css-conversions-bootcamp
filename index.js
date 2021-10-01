b5aa2911a4001191d5b63f038b9435dd33d20903
EricFooSD <fooshida@gmail.com>
2021-10-01 21:51:58 +0800

    base

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+// HEX to RGB
+
+const hex = process.argv[2];
+
+const hexToRgb = (a) => {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(a);
+  const r = parseInt(result[1], 16);
+  const g = parseInt(result[2], 16);
+  const b = parseInt(result[3], 16);
+  const output = `rgd(${r},${g},${b})`;
+  return output;
+};
+
+const newRGB = hexToRgb(hex);
+console.log(newRGB);
