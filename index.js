34d408fee113cfc859901f20cc933858d92916cd
EricFooSD <fooshida@gmail.com>
2021-10-01 22:38:25 +0800

    RBG to HSL

--- index.js
+++ index.js
@@ -14,24 +14,72 @@
 // const newRGB = hexToRgb(hex);
 // console.log(newRGB);
 
-// RGB to HEX
+// // RGB to HEX
+
+// const rgb = process.argv[2];
+
+// const rgbArray = rgb.split("");
+
+// const r = Number(`${rgbArray[4]}${rgbArray[5]}${rgbArray[6]}`);
+// const g = Number(`${rgbArray[8]}${rgbArray[9]}${rgbArray[10]}`);
+// const b = Number(`${rgbArray[12]}${rgbArray[13]}${rgbArray[14]}`);
+
+// const componentToHex = (c) => {
+//   var hex = c.toString(16);
+//   return hex.length == 1 ? "0" + hex : hex;
+// };
+// const newHex =
+//   "#" +
+//   `${componentToHex(r)}` +
+//   `${componentToHex(g)}` +
+//   `${componentToHex(b)}`;
+
+// console.log(newHex);
+
+//RGB to HSL
 
 const rgb = process.argv[2];
 
 const rgbArray = rgb.split("");
 
-const r = parseInt(`${rgbArray[4]}${rgbArray[5]}${rgbArray[6]}`);
-const g = parseInt(`${rgbArray[8]}${rgbArray[9]}${rgbArray[10]}`);
-const b = parseInt(`${rgbArray[12]}${rgbArray[13]}${rgbArray[14]}`);
+const r = Number(`${rgbArray[4]}${rgbArray[5]}${rgbArray[6]}`);
+const g = Number(`${rgbArray[8]}${rgbArray[9]}${rgbArray[10]}`);
+const b = Number(`${rgbArray[12]}${rgbArray[13]}${rgbArray[14]}`);
 
-const componentToHex = (c) => {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-};
-const newHex =
-  "#" +
-  `${componentToHex(r)}` +
-  `${componentToHex(g)}` +
-  `${componentToHex(b)}`;
+function rgbToHsl(r, g, b) {
+  (r /= 255), (g /= 255), (b /= 255);
+  var max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  var h,
+    s,
+    l = (max + min) / 2;
 
-console.log(newHex);
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+    h /= 6;
+  }
+  const H = Math.floor(h * 100);
+  const S = Math.floor(s * 100);
+  const L = Math.floor(l * 100);
+
+  const output = `hsl(${H}%,${S}%,${L}%)`;
+  return output;
+}
+
+const newHSL = rgbToHsl(r, g, b);
+
+console.log(newHSL);
