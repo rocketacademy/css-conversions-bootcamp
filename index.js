ac4448808ff47b87cc47d609250a0dfb02e880d3
GaryTangYC <tangyewchiang@gmail.com>
2021-10-20 15:39:54 +0800

    base

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const hexValues = process.argv[2];
+const hexToRGB = () => {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexValues);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+console.log(hexToRGB(hexValues));
