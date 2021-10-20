32071f907175b643d8eb64c7bcdf582171f40c1f
shannonssd <shannonssd@gmail.com>
2021-10-20 19:57:49 +0800

    Base

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+// Convert hex to RGB
+let hex = process.argv[2]
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? 
+    `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`
+   : null;
+}
+
+console.log(hexToRgb(hex)); 
