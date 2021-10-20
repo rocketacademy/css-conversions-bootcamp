36175a35b8754b203c1555db535950a94d4e1af8
GaryTangYC <tangyewchiang@gmail.com>
2021-10-20 17:55:15 +0800

    comfortable

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const hexValues = process.argv[2];
-const hexToRGB = () => {
-  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexValues);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
+const convName = process.argv[2]
+const hexValues = process.argv[3];
+const rgbValues = process.argv[3];
+let printResult;
+// console.log(convName[0])
+// console.log(hexValues)
+if (convName === 'hexToRGB') {
+  
+  if (hexValues[0] === '#') {
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexValues);
+    let r = parseInt(result[1], 16);
+    let g = parseInt(result[2], 16);
+    let b = parseInt(result[3], 16);    
+    printResult = `rgb(${r},${g},${b})`
+  } else { 
+   console.log('Usage: node index.js hexToRGB (\'\#values\')')
+  }
+};
+
+if (convName === 'rgbToHex') {
+let result = /rgb(\((\d+),(\d+),(\d+)\))/.exec(rgbValues);
+let hexVal = (parseInt(result[2]) << 16) + (parseInt(result[3]) << 8) + parseInt(result[4]);
+printResult = `#${hexVal.toString(16)}` 
 }
-console.log(hexToRGB(hexValues));
+
+console.log(printResult);
