7b9de1a98dd9c94020e035a02caccb6e3680ba2d
Nuuggs <bryluke000@gmail.com>
2021-10-23 12:15:55 +0800

    completed base css conversion

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('hello world');
+// ===== BASE ===== //
+// ----- hex to RGB ----- //
+
+// input: node index.js '#ffffff'
+// output: rgb(255,255,255)
+
+const hexInput = process.argv [2];
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+const rgbOutput = hexToRgb(hexInput);
+console.log(rgbOutput);
+// alert(hexToRgb("#0033ff").g); // "51";
+
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+// alert(rgbToHex(0, 51, 255)); // #0033f
