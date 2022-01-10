c915410795c6df35268aea24fa91ac7a0f1f9ed9
Goh Yong Siang <gysiang@gmail.com>
2022-01-10 21:37:49 +0800

    used export modules on functions.js to index.js

--- index.js
+++ index.js
@@ -1,68 +1,19 @@
 
 ////////////////////////////////////////
-//         Helper Function            //
+//         Global Variables            //
 //////////////////////////////////////
 
+import{
+  hexToRgb,
+  getRgbFromInput,
+  rgbToHex,
+  rgbToHsl,
+  hslToHex,
+  hslToRgb
+} from './functions.js'
+
 let conversionMethod = process.argv[2];
-let red;
-let green;
-let blue;
 
-const hexToRgb = (hex) => {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  result = `rgb(${parseInt(result[1],16)},${parseInt(result[2],16)},${parseInt(result[1],16)})`
-  return result;
-}
-
-const getRgbFromInput = (rgbString) => {
- 
-    // Removing "rgb(" and ")" from the string "rgb(v1, v2, v3)"
-    let tempValue = rgbString.replace('rgb(', '');
-    tempValue = tempValue.replace(')','');
-    // console.log(tempValue);
-    const rgbVal = tempValue.split(',');
-    red = rgbVal[0];
-    green = rgbVal[1];
-    blue = rgbVal[2];
-    return({r:rgbVal[0], g:rgbVal[1], b:rgbVal[2]});
-  };
-
-const componentToHex=(c) => {
-    // Converting the input string value to number and change the base value to hex
-    var hex = Number(c).toString(16);
-    const val = (( hex.length == 1 ) ? "0" + hex : hex);
-    return val;
-  }
-
-const rgbToHex = (r, g, b) => {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-  }
-
-const rgbToHsl = (r,g,b) => {
-  let v=Math.max(r,g,b), c=v-Math.min(r,g,b), f=(1-Math.abs(v+v-c-1)); 
-  let h= c && ((v==r) ? (g-b)/c : ((v==g) ? 2+(b-r)/c : 4+(r-g)/c)); 
-  const result = [60*(h<0?h+6:h), f ? c/f : 0, (v+v-c)/2];
-  return (`hsl(${result[0]},${result[1]}%,${result[2]}%)`)
-}
-
-const hslToHex = (hsl) => {
- let result = /hsl(\((\d+),(\d+),(\d+)\))/.exec(hsl);
-  result[4] /= 100;
-  const a = (result[3] * Math.min(result[4], 1 - result[4])) / 100;
-  const f = (n) => {
-  const k = (n + result[2] / 30) % 12;
-  const color = result[4] - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-  return Math.round(255 * color)
-      .toString(16)
-      .padStart(2, "0"); // convert to Hex and prefix "0" if needed
-  };
-  return `#${f(0)}${f(8)}${f(4)}`;
-}
-
-const hslToRgb = (hsl) =>{
-   let hexValue = hslToHex(hsl);
-   return hexToRgb(hexValue)
-};
 ////////////////////////////////////////////////////////////////////////////////
 
 //node index.js hexToRgb '#ffffff'
@@ -75,29 +26,27 @@ if (conversionMethod == 'hexToRgb') {
 
 //node index.js rgbToHex 'rgb(255,255,255)'
 if (conversionMethod == 'rgbToHex') {
-  let stringRgbInput = process.argv[3];
 
-  getRgbFromInput(stringRgbInput);
-  const convertRgbToHex = rgbToHex(red, green, blue);
-  console.log(convertRgbToHex);
+  let rgbInput = process.argv[3];
+  let convertRGBtoHex = rgbToHex(rgbInput);
+  console.log(convertRGBtoHex);
 }
 
 //node index.js rgbToHsl 'rgb(255,255,255)'
 if (conversionMethod == 'rgbToHsl') {
-  let stringRgbInput = process.argv[3];
+  let rgbInput = process.argv[3];
 
-  getRgbFromInput(stringRgbInput);
-  let convertRgbToHsl = rgbToHsl(red, green, blue);
+  let convertRgbToHsl = rgbToHsl(rgbInput);
   console.log(convertRgbToHsl)
 }
+
 // node index.js hexToHsl '#ffffff'
 if (conversionMethod == 'hexToHsl'){
 
   let hexInput = process.argv[3];
 
-  let rgbValue = hexToRgb(hexInput);
-  getRgbFromInput(rgbValue);
-  let convertRgbToHsl = rgbToHsl(red, green, blue);
+  let convertHexToRgb = hexToRgb(hexInput)
+  let convertRgbToHsl = rgbToHsl(convertHexToRgb);
   
   console.log(convertRgbToHsl)
 
