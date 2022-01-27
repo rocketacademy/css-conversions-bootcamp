8288597d38703fe35c954842cc369be48f532657
tabbiho <tabbiho@gmail.com>
2022-01-27 15:44:52 +0800

    Comfortable completed

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ const hexToRgb = (hex) => {
   } : null;
 }
 
-const rgb = hexToRgb(process.argv[2])
+const componentToHex = (c) => {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+const rgbToHex = (r, g, b) => {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+if (process.argv[2] === 'hexrgb'){
+const rgb = hexToRgb(process.argv[3])
 console.log(`rgb(${rgb.r},${rgb.g},${rgb.b})`)
+} else if (process.argv[2] === 'rgbhex'){
+const rgbArray = process.argv[3].match(/[0-9]+/g).map((x)=>parseInt(x,10))
+console.log(rgbToHex(rgbArray[0], rgbArray[1], rgbArray[2]));
+}
